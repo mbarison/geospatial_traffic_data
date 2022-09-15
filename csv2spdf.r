4bfb3aa92f8cd2449e188f51d8cfc85599a55b5b
Marcello Barisonzi <eltorpe@hotmail.com>
2022-09-15 12:13:32 -0400

    Convert CSV to SpatialPointsDataFrame

---
+++ csv2spdf.r
@@ -0,0 +1,25 @@
+# File: csv2spdf.r
+# Author: Marcello Barisonzi
+# Purpose: import CSV with Traffic Flow data and convert to SpatialPointsDataFrame
+
+library(sp)
+library(geoR)
+library(dplyr)
+
+csv2geodata <- function(csv_file_path, city_name) {
+
+    # read CSV file, select city
+    csv_raw <- filter(read.csv(csv_file_path), traffic_city==city_name)
+
+    # Transform coordinates from string to data pair
+    coords <- cbind( as.numeric(gsub(".*?([-]*[0-9]+[.][0-9]+).*", "\\1", csv_raw$WKT)),
+                     as.numeric(gsub(".* ([-]*[0-9]+[.][0-9]+).*", "\\1", csv_raw$WKT)))
+    csv_raw$WKT <- NULL
+
+    # Create SpatialPointsDataFrame from coordinates and data
+    sp_dat = SpatialPointsDataFrame( coords=coords, 
+                                     proj4string=CRS("+proj=longlat +datum=WGS84"), 
+                                     data=as.data.frame(csv_raw) )
+
+    return(sp_dat)
+}
